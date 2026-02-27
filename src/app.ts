d6c8526bfc9f7dac071902dc4a0c6568a516c9ab
NayanMishra <nayanmishra.279@gmail.com>
2026-02-28 01:57:59 +0530

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
 import express from "express";
 import identifyRoute from "./routes/identify.route";
-import path from "path/win32";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(process.cwd(), "public")));
 app.get("/", (req, res) => {
   res.status(200).send("Bitespeed Identity Reconciliation API is running perfectly!");
-});
-
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "API is active and running!" });
 });
 
 app.use("/identify", identifyRoute);
