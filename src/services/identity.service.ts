cdf6eea164ead70cc9ad47e782c23824a184d383
NayanMishra <nayanmishra.279@gmail.com>
2026-02-28 01:03:20 +0530

    fix: use native prisma client to resolve SSL connection errors

--- src/services/identity.service.ts
+++ src/services/identity.service.ts
@@ -1,13 +1,10 @@
-import { PrismaClient, Prisma } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
+
 import type { Contact } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
 
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 
 
