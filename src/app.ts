a7915e984196c2deaedab8e305c02dee3604604c
NayanMishra <nayanmishra.279@gmail.com>
2026-02-28 01:31:36 +0530

    feat: add health check route for root URL

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import identifyRoute from "./routes/identify.route";
 const app = express();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).send("Bitespeed Identity Reconciliation API is running perfectly!");
+});
+
 app.use("/identify", identifyRoute);
 
 export default app;
